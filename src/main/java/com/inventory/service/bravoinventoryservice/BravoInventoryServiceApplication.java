1ac000e815972b29a4eb9987153707b7dc632bfb
tomriteymurlu <tomristt9@gmail.com>
2026-07-22 18:00:56 +0400

    Initial project setup

---
+++ src/main/java/com/inventory/service/bravoinventoryservice/BravoInventoryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.inventory.service.bravoinventoryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BravoInventoryServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BravoInventoryServiceApplication.class, args);
+    }
+
+}
